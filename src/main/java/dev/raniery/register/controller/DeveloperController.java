061481cf511deea5befaff5aed4fadc03a0c1379
Ranieeery <raniery2003@hotmail.com>
2025-04-28 16:58:59 -0300

    feat: implement template CRUD operations for Developer entity in DeveloperController

--- src/main/java/dev/raniery/register/controller/DeveloperController.java
+++ src/main/java/dev/raniery/register/controller/DeveloperController.java
@@ -1,8 +1,33 @@
 package dev.raniery.register.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/developer")
 public class DeveloperController {
 
+    @PostMapping
+    public String createDeveloper() {
+        return "Developer created";
+    }
+
+    @GetMapping("/all")
+    public String listDeveloper() {
+        return "List of developers";
+    }
+
+    @GetMapping("/all/{id}")
+    public String listDeveloperById() {
+        return "Developer by id";
+    }
+
+    @PutMapping
+    public String updateDeveloper() {
+        return "Developer updated";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteDeveloper() {
+        return "Developer deleted";
+    }
 }
