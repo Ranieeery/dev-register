28c518fb775fd9a20ad9180f96d87caa280c08cc
Ranieeery <raniery2003@hotmail.com>
2025-04-29 10:04:54 -0300

    refactor: remove unused import statement in DeveloperService

--- src/main/java/dev/raniery/register/service/DeveloperService.java
+++ src/main/java/dev/raniery/register/service/DeveloperService.java
@@ -2,7 +2,6 @@ package dev.raniery.register.service;
 
 import dev.raniery.register.model.developer.Developer;
 import dev.raniery.register.repository.DeveloperRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
